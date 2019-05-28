789127dc4d44a141d74959b4d066191d8bedef12
alignwaivers <alignwaivers@gmail.com>
2019-05-27 23:52:11 -0700

    interim resolved

--- gbeatstation_main.py
+++ gbeatstation_main.py
@@ -1,7 +1,7 @@
 import rtmidi2, jackmatchmaker, subprocess, atexit, time, os, random
 from pythonosc import udp_client, dispatcher, osc_server
 
-#project specific internals
+# project specific internals
 import gridmaster, openstagec, jackconnect
 from slooper import *
 
@@ -9,61 +9,47 @@ def callback_midi(note, time_stamp):
     chan, note, vel = note
     vel = 0 if vel <= 64 else 1
     if chan == 176:
-        print (note, vel)
+        print(note, vel)
         x = note - 104
         y = 8
     else:
-        x = ( note - 1) % 10
-        y = ( 99 - note ) // 10
+        x = (note - 1) % 10
+        y = (99 - note) // 10
         y = -y + 8
-        
-    coordinate(x,y, vel)
+
+    coordinate(x, y, vel)
+
 
 def coordinate(x, y, vel):
     if Grid.swap != None:
         if vel == True:
-            if Grid.swap == "add": #swapping button functions dynamically
+            if Grid.swap == "add":  # swapping button functions dynamically
                 Grid.addaction["function"] = Grid.fgrid[x, y][Grid.mode][0][vel]
                 Grid.addaction["args"] = Grid.fgrid[x, y][Grid.mode][1][vel]
                 Grid.addaction["color"] = Grid.pgrid[x, y][Grid.mode][vel]
-                print ('addict', Grid.addaction)
+                print('addict', Grid.addaction)
             elif Grid.swap == "implement":
-                print ('implementing')
-                print (x, y, Grid.addaction["function"], Grid.addaction["args"])
-                Grid.alter_pressfunc(x, y, vel, func = Grid.addaction["function"], args=Grid.addaction["args"],
-                                     color= [63, 63, 63])#Grid.addaction["color"])
+                print('implementing')
+                print(x, y, Grid.addaction["function"], Grid.addaction["args"])
+                Grid.alter_pressfunc(x, y, vel, func=Grid.addaction["function"], args=Grid.addaction["args"],
+                                     color=[63, 63, 63])  # Grid.addaction["color"])
                 Grid.buttonclrchange(x, y, False, Grid.addaction["color"])
 
     else:
-        if y < 8 and x < 8 and vel == True: #pulse when pressed
+        if y < 8 and x < 8 and vel == True:  # pulse when pressed
             var = ["pulse", 119]
-        else:  var = None
+        else:
+            var = None
 
-        if (x,y) not in Grid.pressed:
+        if (x, y) not in Grid.pressed:
             Grid.pressed[x, y] = True
-        elif (x,y) in Grid.pressed:
-            del Grid.pressed[x,y]
-
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
-        #if Grid.mode == "lplay":
-            #each button must hold values for state, sync, activated/not
-        #=    pass
->>>>>>> Stashed changes
+        elif (x, y) in Grid.pressed:
+            del Grid.pressed[x, y]
 
         if x == 8:
-=======
-        #if Grid.mode == "lplay":
-            #each button must hold values for state, sync, activated/not
-        #=    pass
-
-
-        elif x == 8:
->>>>>>> Stashed changes
             if Grid.mode in ["loop", 'rand']:
                 loopsync = Slmast.loops[y].sync
-                syncclr = [0, 0, 0] if loopsync == True else [0,0,63]
+                syncclr = [0, 0, 0] if loopsync == True else [0, 0, 63]
                 if vel == True:
                     Grid.ledout(8, y, syncclr, temp=True)
                     Slmast.loops[y].sync = not Slmast.loops[y].sync
@@ -73,7 +59,7 @@ def coordinate(x, y, vel):
 
 
                 elif vel == False:
-                    Grid.ledout(8, y, Grid.pgrid[x,y]["current"], temp=True)
+                    Grid.ledout(8, y, Grid.pgrid[x, y]["current"], temp=True)
 
             elif Grid.mode in ["instr"]:
                 if vel == True:
@@ -91,21 +77,22 @@ def coordinate(x, y, vel):
                 if x < 4:
                     Grid.switchmode(Grid.modelst[x])
                 elif x == 4:
-                    for y in range(8): # consider directionality
+                    for y in range(8):  # consider directionality
                         if Slmast.loops[y].state != 14:
                             loop_pause(y)
 
-        else: # 8x8 grid
+        else:  # 8x8 grid
             Grid.ledout(x, y, Grid.pgrid[x, y][Grid.mode][vel], var)
             if Grid.mode == "loop":
                 sl_loopmode_cmd(x, y, vel)
             elif Grid.mode == "instr":
                 var = None
                 midinote = x + (y * 8) + 12
-                print (midinote)
+                print(midinote)
                 glass_instr.send_noteon(144, midinote, vel * 127)
             elif Grid.mode == "rand":
                 Grid.gridpress(x, y, vel)
+
 
 def sl_loopmode_cmd(x, y, vel):
     yinv = y
@@ -126,25 +113,25 @@ def sl_loopmode_cmd(x, y, vel):
                 Slmast.loops[y].sync = True
 
             slclient.send("/sl/{}/down".format(str(y)), "oneshot")
-            #Grid.ledrow(1, 90)
-            
-        elif vel == False:# 'oneshot' release is a pause
+            # Grid.ledrow(1, 90)
+
+        elif vel == False:  # 'oneshot' release is a pause
             if lp.state != 14:
                 loop_pause(y)
-                #Grid.ledrow(1, 0)
+                # Grid.ledrow(1, 0)
         Grid.ledout(0, yinv, Grid.pgrid[0, yinv][Grid.mode][vel])
 
     elif vel == True:
-        slclient.send("/sl/{}/down".format(str(y)), Slmast.cmds[x])            
+        slclient.send("/sl/{}/down".format(str(y)), Slmast.cmds[x])
 
 
 def loop_pause(y):  # aka mute pause for the weird states
     if Slmast.loops[y].state != 14:
         slclient.send("/sl/{}/down".format(str(y)), "mute")
         slclient.send("/sl/{}/down".format(str(y)), "pause")
-        #reset loop_pos to start?
+        # reset loop_pos to start?
 
-            
+
 class OSC_Sender():
     def __init__(self, ipaddr="127.0.0.1", port=9951):
         self.osc_client = udp_client.SimpleUDPClient(ipaddr, port)
@@ -153,59 +140,54 @@ class OSC_Sender():
         self.osc_client.send_message(addr, arg)
 
 
-
-
-#should commands to sooperlooper be midi driven for expediency? if less bytes, diff bottleneck
+# should commands to sooperlooper be midi driven for expediency? if less bytes, diff bottleneck
 
 
 if __name__ == "__main__":
-    Mk2_in = rtmidi2.MidiIn("mk2-launchpad") #midi input AND output port combined
-    Mk2_in.open_virtual_port("mk2-in")  
+    Mk2_in = rtmidi2.MidiIn("mk2-launchpad")  # midi input AND output port combined
+    Mk2_in.open_virtual_port("mk2-in")
     Mk2_in.callback = callback_midi
-    Mk2_out = rtmidi2.MidiOut("mk2-launchpad") # midi output
+    Mk2_out = rtmidi2.MidiOut("mk2-launchpad")  # midi output
     Mk2_out.open_virtual_port("mk2-out")
 
-    #instrument
-    glass_instr = rtmidi2.MidiOut("glass_instrument") # midi output
+    # instrument
+    glass_instr = rtmidi2.MidiOut("glass_instrument")  # midi output
     glass_instr.open_virtual_port("glass_instrument")
 
-    glass_drum = rtmidi2.MidiOut("glass_drum") # midi output
+    glass_drum = rtmidi2.MidiOut("glass_drum")  # midi output
     glass_drum.open_virtual_port("glass_drum")
 
-    #midiccouts
-    glass_cc = rtmidi2.MidiOut("glass_cc") # midi output
+    # midiccouts
+    glass_cc = rtmidi2.MidiOut("glass_cc")  # midi output
     glass_cc.open_virtual_port("glass_cc")
 
-    #sequencer
-    glass_seq = rtmidi2.MidiOut("glass_sequencer") # midi output
+    # sequencer
+    glass_seq = rtmidi2.MidiOut("glass_sequencer")  # midi output
     glass_seq.open_virtual_port("glass_sequencer")
 
     jack = jackconnect.JackConnections()
-      
+
     slclient = OSC_Sender(ipaddr="127.0.0.1", port=9951)
     stage_osc = OSC_Sender(ipaddr="127.0.0.1", port=8080)
-    
+
     Grid = gridmaster.Gridmaster(stage_osc, Mk2_out, glass_cc)
-
     Slmast = Slmaster(Grid, slclient)
-
-    invlps = [Sloop(Grid,slclient) for i in range(8)] #instantiate invidual loops
-    Slmast.loops = invlps[::-1]
-
     OStageC = openstagec.OpenStageControl(Grid, coordinate, Slmast, stage_osc, glass_cc, jack)
 
+    invlps = [Sloop(Grid, slclient) for i in range(8)]
+    Slmast.loops = invlps[::-1]
 
     for y in range(4):
-        clr = Slmast.loops[y].color # this is confusing..
-        y = -y + 7 #invert  y
-        for x in range(8):   # setup 'rand' mode functionalityy
+        clr = Slmast.loops[y].color  # this is confusing..
+        y = -y + 7  # invert  y
+        for x in range(8):  # setup 'rand' mode functionalityy
             Grid.alter_pressfunc(x, y, True, func=sl_loopmode_cmd, args=[x, y, 1], color=clr)
             if x == 2:
                 Grid.alter_pressfunc(x, y, False, func=sl_loopmode_cmd, args=[x, y, 0])
-            Grid.pgrid[x,y]['rand'][True] = clr
-            #else:  # could probably reduce this redundancy and get ri["^a2j:glass_cc", "^Bitrot"],d of if/else
-                #Grid.alter_pressfunc(i, y, False)
-                #??
+            Grid.pgrid[x, y]['rand'][True] = clr
+            # else:  # could probably reduce this redundancy and get ri["^a2j:glass_cc", "^Bitrot"],d of if/else
+            # Grid.alter_pressfunc(i, y, False)
+            # ??
 
     for x in range(4):
         y = 8
@@ -215,59 +197,57 @@ if __name__ == "__main__":
         for y in range(4):
             note = x + (y * 4) + 36
             for i in True, False:
-                Grid.alter_pressfunc(x + 4, y, i, func=glass_drum.send_noteon, args =[144, note, i * 127], color = [30,20,15])
-
-
+                Grid.alter_pressfunc(x + 4, y, i, func=glass_drum.send_noteon, args=[144, note, i * 127],
+                                     color=[30, 20, 15])
 
     for y in range(4):  # add oneshot buttons
         x = 3
         clr = Slmast.loops[-y + 7].color
-        Grid.alter_pressfunc(x, y, True, func=sl_loopmode_cmd, args=[2,y, True])
-        Grid.alter_pressfunc(x, y, False, func=sl_loopmode_cmd, args=[2,y, False], color=clr)
+        Grid.alter_pressfunc(x, y, True, func=sl_loopmode_cmd, args=[2, y, True])
+        Grid.alter_pressfunc(x, y, False, func=sl_loopmode_cmd, args=[2, y, False], color=clr)
 
     for y in range(4):  # add oneshot buttons
         x = 2
         loop = y + 4
         clr = Slmast.loops[-loop + 7].color
-        Grid.alter_pressfunc(x, y, True, func=sl_loopmode_cmd, args=[2,loop, True])
-        Grid.alter_pressfunc(x, y, False, func=sl_loopmode_cmd, args=[2,loop, False], color=clr)
+        Grid.alter_pressfunc(x, y, True, func=sl_loopmode_cmd, args=[2, loop, True])
+        Grid.alter_pressfunc(x, y, False, func=sl_loopmode_cmd, args=[2, loop, False], color=clr)
 
-    cc_clr = [0,0,13]
+    cc_clr = [0, 0, 13]
 
     for i in range(4):
         invi = -i + 3
         Grid.alter_pressfunc(0, invi, True, func=[Grid.bitrotchange],
-                             args=[[True, i * 31], None], color = [63, 63, 63])  # estimated good cc note values
+                             args=[[True, i * 31], None], color=[63, 63, 63])  # estimated good cc note values
 
-        Grid.alter_pressfunc(0, invi, False, func=[Grid.bitrotchange], args=[[False, 0],None],
+        Grid.alter_pressfunc(0, invi, False, func=[Grid.bitrotchange], args=[[False, 0], None],
                              color=[30 + c * i for c in cc_clr])
 
-
-    toprow = ['loop', 'instr', 'lplay', 'custom', 'pause', ' ', ' ', ' ']  #automap controls labels
+    toprow = ['loop', 'instr', 'lplay', 'custom', 'pause', ' ', ' ', ' ']  # automap controls labels
     for y in range(8):
         OStageC.send('/column_text/' + str(y), str(y))
         OStageC.send('/automap_text/' + str(y), toprow[y])
 
-                          
-# osc server handlers
+    # osc server handlers
     dispatcher = dispatcher.Dispatcher()
     dispatcher.map("/sloop", Slmast.sloschandler)  # sooperlooper handler
-    dispatcher.set_default_handler(OStageC.stage_handler)    
+    dispatcher.set_default_handler(OStageC.stage_handler)
     server = osc_server.ThreadingOSCUDPServer(("localhost", 9998), dispatcher)
 
     for mde in Grid.modelst:
         Grid.switchmode(mde)
         time.sleep(.25)
-      
+
+
     def exit_handler():
-        print ('exiting')
+        print('exiting')
         Grid.reset()
+
 
     slclient.send("/ping", ["localhost:9998", "/sloop"])
     for i in range(8):
         slclient.send("/sl/{}/get".format(i), ["loop_len", "localhost:9998", "/sloop"])
 
-
     atexit.register(exit_handler)
     server.serve_forever()  # blocking osc server=
-    
+
