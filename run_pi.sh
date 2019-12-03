3c30519d8d35b7b8cff7f02dd8a84cbdfe3b5f6a
alignwaivers <alignwaivers@gmail.com>
2019-12-03 14:59:35 -0800

    initial_connections system_capture -> sooperlooper ->system_playback

--- run_pi.sh
+++ run_pi.sh
@@ -2,8 +2,14 @@
 trap "exit" INT TERM ERR
 trap "kill 0" EXIT
 
+hydrogen &
+
+zynaddsubfx &
+
 sooperlooper -L ./interfaces/default.slsess -l 8 -m ./interfaces/sl_bindings.slb &
 
 node /home/pi/installed_local/open-stage-control/app/ -n -l interfaces/stagecontrol.json  -s 127.0.0.1:9998 -m "open-stage-cc:virtual" &
+
+sleep 5 &&
 
 python3 gbeatstation_main.py
