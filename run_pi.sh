b3dc6ab4ac700ea8855d9701c781084c5d7a18f2
alignwaivers <alignwaivers@gmail.com>
2020-03-06 01:56:52 -0800

    updated layout openstage control

--- run_pi.sh
+++ run_pi.sh
@@ -7,7 +7,7 @@ jack_control start && a2j_control start && jack_bufsize 400 &&
 
 ./interfaces/jackmatch.sh &
 
-#ardour5 /home/pi/Music/ardour/pinauguration
+#ardour5 /home/pi/Music/ardour/pinauguration &&
 
 sooperlooper -L /home/pi/projects/glass_beatstation/interfaces/default.slsess -l 8 -m /home/pi/projects/glass_beatstation/interfaces/sl_bindings.slb &
 
@@ -15,7 +15,7 @@ sleep 2 &&
 
 node /home/pi/installed_local/open-stage-control/app/ -n -l /home/pi/projects/glass_beatstation/interfaces/stagecontrol.json  -s 127.0.0.1:9998 -m "open-stage-cc:virtual" &
 
-#cd /home/pi/installed_local/VL53L0X_rasp_python/python/ && python3 ./vlidar_midi.py &
+cd /home/pi/installed_local/VL53L0X_rasp_python/python/ && python3 ./vlidar_midi.py &
 
 sleep 10 &&
 
