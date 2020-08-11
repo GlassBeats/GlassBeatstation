af16646dabc0aee262c126a25d676a234cab2166
alignwaivers <alignwaivers@gmail.com>
2020-08-11 00:44:00 -0700

    fixed jack-matchmaker execution problem by bypassing bin default install to .local/bin/jack-matchmaker and fixed launchpad connecting to itself

--- interfaces/jackmatch.sh
+++ interfaces/jackmatch.sh
@@ -2,15 +2,16 @@
 trap "exit" INT TERM ERR
 trap "kill 0" EXIT
 
-jack-matchmaker ^a2j:open-stage-cc ^ardour:MIDI \
+cd  &&
+.local/bin/jack-matchmaker \
+                ^a2j:mk2 ^a2j:Launchpad \
+                ^a2j:Launchpad ^a2j:mk2 \
+                ^a2j:open-stage-cc ^ardour:MIDI \
                 ^a2j:open-stage-cc ^a2j:sooperlooper \
                 .*glass_cc ^a2j:sooperlooper \
                 .*glass_drum ^ardour:Drum \
                 .*glass_sequencer ^ardour:Drum \
                 .*glass_cc ^ardour:MIDI control \
                 ^a2j:open-stage ^a2j:sooperlooper \
-                ^a2j:mk2 ^a2j:Launchpad \
-                ^a2j:Launchpad ^a2j:mk2 \
 
-                
                 
