00bf052b5a056de878bd2e0560b93e1f97eb5f9f
linuxbeatsall <linuxbeatsall@gmail.com>
2020-11-03 13:21:51 -0800

    multifunctionality for each Button class and Button.checkfuncs

--- glassbeats.py
+++ glassbeats.py
@@ -7,9 +7,7 @@ class Grid(object):
         for x in range(rows):
             for y in range(columns):
                 self.Buttons[(x,y)] = Button(self.mode, (x, y))
-    def __repr__(self):
-        pass
-                 
+                
 class Button (Grid):
     def __init__(self, mode, coord):
         self.coord = coord
@@ -17,51 +15,51 @@ class Button (Grid):
         self.state = 0
         self.color = [{}, {}] #this should be stackable, last off
 
-
     def __str__(self):
         return (str(self.coord) + str(self.funcs))
 
-    def replace_func(self, vel, function, mode=None):
-        if mode == None: mode = self.mode  
+    def checkfuncs(self, vel, mode):
         try:
-            if self.funcs[vel][mode]:
-                print ('{}[vel={}][mode={}] replacing {} with {}'.format(self.coord, vel, mode, self.funcs[vel][mode], function))
-                self.funcs[vel][mode] = function
-
+            if isinstance(self.funcs[vel][mode], list) == True:
+                return len(self.funcs[vel][mode])
+            else: return 1
         except KeyError:
-                print ('{}[vel={}][mode={}] has no function yet, use add_func'.format(self.coord, vel, mode))
+            return 0
+
+    def replace_func(self, vel, function, mode=None):
+        if mode == None: mode = self.mode
+        if self.checkfuncs(vel, mode) > 1:
+            print (self, 'replacing {} with {}'.format(self.funcs[vel][mode], function))
+            self.funcs[vel][mode] = function
+        elif self.checkfuncs(vel,mode) == 1:
+                print (self,'no function to replace, use add_func')
                        
     def add_func(self, vel, function, mode=None):
         if mode == None: mode = self.mode
-        print ('*' * 20)
-        print (self)
-        print ('*' * 20)
-        try: 
-            if isinstance(self.funcs[vel][mode], list):
-                    self.funcs[vel][mode].append(function)
-            elif self.funcs[vel][mode]:
-                print ('adding {} to currrent action {}'.format)
-        except KeyError:        
-            print ('{}[vel={}][mode={}]: adding function '.format(self.coord, vel, mode, function))
+        numfuncs = self.checkfuncs(vel,mode)
+        if numfuncs == 0:
+            print (self,' : adding function ', function)
             self.funcs[vel][mode] = function
+        elif numfuncs == 1:
+            print (self, 'adding {} to current action'.format(function))
+            self.funcs[vel][mode] = ([self.funcs[vel][mode],function])
+        elif numfuncs > 2:
+            print (self, 'adding {} to current actions {}'.format(function))
+            self.funcs[vel][mode].append(function)
+
         
     def press(self, vel, mode=None):
         if mode == None: mode = self.mode
-        try: 
-            action = self.funcs[vel][mode]
-            print (self.coord, action, end= ' ')
-            action()
-        except KeyError:
-            print ('there is no assigned function yet')
-            
-    def led(index, color, special=None):
-        midiout.write(color)
-        if color:
-            #do special color stuff
-            pass
-    def changeclr(xy, mode, velocity, color):
-        pass
-        
+        numfuncs = self.checkfuncs(vel, mode)
+        if numfuncs == 0: print ('there is no assigned action yet')
+        elif numfuncs == 1:
+            print (self.coord, self.funcs[vel][mode])
+            self.funcs[vel][mode]()
+        elif numfuncs > 1:
+            for a in self.funcs[vel][mode]:
+                a()
+
+
 def testfunc():
     print ('first action')
 
@@ -72,6 +70,9 @@ if __name__ == '__main__':
     Matrix = Grid(5,5)
     Matrix.Buttons[1,1,].add_func(True, testfunc)
     Matrix.Buttons[1,1,].press(True)
+    print ('*********')
+
     
     Matrix.Buttons[1,1,].add_func(True, testfunc2)
-    Matrix.Buttons[1,1,].press(True)  
+    Matrix.Buttons[1,1,].press(True)
+    print(Matrix)
